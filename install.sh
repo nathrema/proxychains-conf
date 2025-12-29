3fd1fa54d64465e0d1f9b338c31b2a5305d8a5a7
nathrema <stephenwpoole@gmail.com>
2025-12-29 16:57:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 INSTALL=/opt/proxychains-conf
 mkdir $INSTALL
 cd $INSTALL
-wget https://raw.githubusercontent.com/nathrema/proxychains-conf/refs/heads/main/install.sh
+wget https://raw.githubusercontent.com/nathrema/proxychains-conf/refs/heads/main/proxychains-conf.py
 ln -s /usr/sbin/proxychains-conf $INSTALL/proxychains-conf.py
 chmod +x $INSTALL/proxychains-conf.py
